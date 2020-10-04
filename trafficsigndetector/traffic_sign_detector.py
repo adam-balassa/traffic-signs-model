299c83af38eafe026e67a8a4878c2159577b6fa0
Adam Balassa <balassaadi@gmail.com>
2020-10-05 00:44:59 +0200

    recurring

--- trafficsigndetector/traffic_sign_detector.py
+++ trafficsigndetector/traffic_sign_detector.py
@@ -1,14 +1,14 @@
-from classifier.classifier import TrafficSignClassifier
+from detector import ObjectDetector
+from recurring_classifier.classifier import Classifier
+from utils import time
 from .bounding_box import extend_bounding_boxes
 from .process_images import prepare_for_classification
-from detector import ObjectDetector
-from utils import time
 
 
 class TrafficSignDetector(object):
     def __init__(self):
         self.detector = ObjectDetector()
-        self.classifier = TrafficSignClassifier()
+        self.classifier = Classifier()
 
     def detect(self, image):
         objects = time.measure(lambda: self.detector.predict(image), 'detection')
